c4886d03b28e9f4da4b921a713ddb257bc9fd04f
Russell Mackenzie <russmackdev@gmail.com>
2019-11-04 21:40:25 +0000

    Add initial result tui.

--- src/main.rs
+++ src/main.rs
@@ -14,34 +14,81 @@ use std::time::Duration;
 use clap::{Arg, App};
 use termcolor::{Color, ColorChoice, ColorSpec, StandardStream, WriteColor};
 
-fn exit_usage(s: String) {
+fn print_line_info_prefix(icon: &str, status: &str, message: &str, colour_primary: Color, colour_secondary: Color) {
+    // Display icon.
+    print_string(icon, colour_primary, false);
+
+    // Display status left brace.
+    let left_brace = " [";
+    print_string(left_brace, colour_secondary, false);
+    
+    // Display status word.
+    print_string(status, colour_primary, false);
+
+    // Display status right brace.
+    let right_brace = "]\t";
+    print_string(right_brace, colour_secondary, false);
+    
+    // Display message.
+    print_string(message, colour_secondary, false);
+}
+
+fn print_string(s: &str, colour: Color, bold: bool) {
     let mut stdout = StandardStream::stdout(ColorChoice::Always);
-    match stdout.set_color(ColorSpec::new().set_fg(Some(Color::Yellow))) {
+
+    let mut colour_spec = ColorSpec::new();
+    colour_spec.set_fg(Some(colour));
+    colour_spec.set_bold(bold);
+
+    match stdout.set_color(&colour_spec) {
         Ok(_) => (),
         Err(e) => println!("Failed setting terminal output colour: {}", e),
     };
-
-    let s_out = format!("\n\n{}\nUsage: `lifxcmd --help`", s);
-    if writeln!(&mut stdout,"{}", s_out).is_err() {
-        println!("{}", s_out);
+    let s_out = format!("{}", s);
+    if write!(&mut stdout,"{}", s_out).is_err() {
+        print!("{}", s_out);
     };
+}
+
+fn print_response_header() {
+    println!("\n");
+    print_string("== ", Color::Cyan, false);
+    print_string("Result", Color::White, true);
+    print_string(" ===========================================================================", Color::Cyan, false);
+    println!("\n");
+}
+
+fn exit_usage(s: String) {
+    print_response_header();
+
+    // Display error.
+    print_line_info_prefix("!", "invalid", &s, Color::Yellow, Color::White);
+
+    // Display usage.
+    println!("");
+    let s_out = format!("lifxcmd --help");
+    print_line_info_prefix("→", "usage", &s_out, Color::Cyan, Color::White);
+    println!("\n");
 
     process::exit(1);
 }
 
 fn exit_error(s: String) {
-    let mut stdout = StandardStream::stdout(ColorChoice::Always);
-    match stdout.set_color(ColorSpec::new().set_fg(Some(Color::Red))) {
-        Ok(_) => (),
-        Err(e) => println!("Failed setting terminal output colour: {}", e),
-    };
+    print_response_header();
 
-    let s_out = format!("\n\n{}", s);
-    if writeln!(&mut stdout,"{}", s_out).is_err() {
-        println!("{}", s_out);
-    };
-    
+    print_line_info_prefix("✘", "error", &s, Color::Red, Color::White);
+    println!("\n");
+
     process::exit(1);
+}
+
+fn exit_done() {
+    print_response_header();
+
+    print_line_info_prefix("∗", "Done", "", Color::Green, Color::White);
+    println!("\n");
+
+    process::exit(0);
 }
 
 fn main() {
@@ -290,6 +337,8 @@ fn main() {
     // thread::sleep(Duration::from_millis((fade_len + 1) as u64));
     // Fade device back to initial state.
     // fade(&device, initial_state.unwrap(), 3000);
+
+    exit_done();
 }
 
 fn get_device_state(device: Device) -> Device {
